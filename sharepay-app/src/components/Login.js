cc758a3c9f182395a1bbfc8b066a17e30656bdeb
MavelSterling <mavelynsl@gmail.com>
2023-10-12 10:16:16 -0500

    Se crea la componente para el login

---
+++ sharepay-app/src/components/Login.js
@@ -0,0 +1,65 @@
+// src/components/Login.js
+
+import React, { useState } from 'react';
+import { Container, Paper, Grid, Typography, TextField, Button, Link } from '@mui/material';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Aquí deberías autenticar con tu backend o sistema de autenticación.
+    console.log('Intento de inicio de sesión con:', username, password);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+        <Paper elevation={3} style={{ padding: '20px', marginTop: '20vh' }}>
+            {/* Logo */}
+            <Grid container justify="center" style={{ marginBottom: '20px' }}>
+                <img src="path_to_your_logo.svg" alt="App Logo" width={100} />
+            </Grid>
+
+            <Typography variant="h5" align="center">Iniciar sesión</Typography>
+            <form onSubmit={handleLogin}>
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    label="Usuario"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    label="Contraseña"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button 
+                    type="submit" 
+                    fullWidth 
+                    variant="contained" 
+                    color="primary" 
+                    style={{ marginTop: '20px' }}
+                >
+                    Ingresar
+                </Button>
+                <Grid container justify="center" style={{ marginTop: '20px' }}>
+                    <Link href="/register" variant="body2">
+                        ¿No tienes una cuenta? Regístrate
+                    </Link>
+                </Grid>
+            </form>
+        </Paper>
+    </Container>
+    );
+}
+
+export default Login;
