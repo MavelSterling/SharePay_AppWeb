0a7ae69444f1f20cca9f0cb1542f5f4b606f8f3d
MavelSterling <mavelynsl@gmail.com>
2023-10-14 00:16:01 -0500

    Se configura las rutas

--- sharepay-app/src/App.js
+++ sharepay-app/src/App.js
@@ -1,11 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/dashboard/Dashboard';
+import UserInformation from './components/dashboard/UserInformation';
 import Login from './components/Login';
 import Register from './components/Register';
 import HomePage from './components/HomePage';
 import './App.css';
-
-
 
 function App() {
   return (
@@ -13,8 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/user-information" element={<UserInformation />} />
       </Routes>
     </Router>
   );
