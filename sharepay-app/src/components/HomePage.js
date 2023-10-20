fadb97a5d5e86f171922688193996386e9123090
MavelSterling <mavelynsl@gmail.com>
2023-10-19 20:12:06 -0500

    Para que el home sea responsive

--- sharepay-app/src/components/HomePage.js
+++ sharepay-app/src/components/HomePage.js
@@ -4,20 +4,50 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
     return (
-        <div style={{ backgroundColor: '#ADD8E6', minHeight: '100vh', padding: '50px 0', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-
+        <div style={{ 
+            backgroundColor: '#ADD8E6', 
+            minHeight: '100vh', 
+            padding: '50px 0', 
+            display: 'flex', 
+            flexDirection: 'column', 
+            justifyContent: 'center' 
+        }}>
             {/* Contenido centralizado */}
-            <div style={{ textAlign: 'center', margin: '0 auto', maxWidth: '500px' }}>
-                <img src={logo} alt="SharePay Logo" width={300} className="rotating-logo" style={{ marginBottom: '40px' }} />
-                <h1>Bienvenidos a SharePay App</h1>
+            <div style={{ 
+                textAlign: 'center', 
+                margin: '0 auto', 
+                maxWidth: '90%', 
+                padding: '0 5%'  // Padding horizontal
+            }}>
+                <img 
+                    src={logo} 
+                    alt="SharePay Logo" 
+                    width="80%"  // Usar porcentaje para adaptabilidad
+                    className="rotating-logo" 
+                    style={{ marginBottom: '40px' }} 
+                />
+                <h1 style={{ fontSize: '1.5rem' }}>Bienvenidos a SharePay App</h1>
             </div>
 
             {/* Cabecera: Opciones */}
-            <div style={{ position: 'absolute', top: '20px', right: '50px', display: 'flex', gap: '20px' }}>
-                <Link to="/login" style={{ textDecoration: 'none', color: '#003366' }}>  {/* Azul oscuro */}
+            <div style={{ 
+                position: 'absolute', 
+                top: '20px', 
+                right: '20px',  // Disminuir espacio de margen derecho
+                display: 'flex', 
+                flexDirection: 'column',  // Cambiar dirección a columna para visualización vertical
+                gap: '20px' 
+            }}>
+                <Link 
+                    to="/login" 
+                    style={{ textDecoration: 'none', color: '#003366', fontSize: '1rem' }}  // Reducir tamaño de fuente
+                >
                     Iniciar sesión
                 </Link>
-                <Link to="/register" style={{ textDecoration: 'none', color: '#003366' }}>  {/* Azul oscuro */}
+                <Link 
+                    to="/register" 
+                    style={{ textDecoration: 'none', color: '#003366', fontSize: '1rem' }}  // Reducir tamaño de fuente
+                >
                     Registrarse
                 </Link>
             </div>
