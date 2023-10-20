d0c0339b1aa66e6aa651369166edabe49bc6ecd1
MavelSterling <mavelynsl@gmail.com>
2023-10-19 22:16:22 -0500

    Se actualiza la página de inicio

--- sharepay-app/src/components/HomePage.js
+++ sharepay-app/src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/Logo.png';  // Logo
+import logo from '../assets/Logo.png';
 import { Link } from 'react-router-dom';
 
 function HomePage() {
@@ -17,38 +17,35 @@ function HomePage() {
                 textAlign: 'center', 
                 margin: '0 auto', 
                 maxWidth: '90%', 
-                padding: '0 5%'  // Padding horizontal
+                padding: '0 5%'
             }}>
                 <img 
                     src={logo} 
                     alt="SharePay Logo" 
-                    width="80%"  // Usar porcentaje para adaptabilidad
+                    width="70%"
                     className="rotating-logo" 
-                    style={{ marginBottom: '40px' }} 
+                    style={{ marginBottom: '35px' }} 
                 />
-                <h1 style={{ fontSize: '1.5rem' }}>Bienvenidos a SharePay App</h1>
+                <h1 style={{ fontSize: '2rem' }}>Bienvenidos a SharePay App</h1>
             </div>
 
             {/* Cabecera: Opciones */}
             <div style={{ 
                 position: 'absolute', 
                 top: '20px', 
-                right: '20px',  // Disminuir espacio de margen derecho
-                display: 'flex', 
-                flexDirection: 'column',  // Cambiar dirección a columna para visualización vertical
-                gap: '20px' 
+                right: '20px',
+                display: 'flex',
+                gap: '10px'  // Pequeño espacio entre botones
             }}>
-                <Link 
-                    to="/login" 
-                    style={{ textDecoration: 'none', color: '#003366', fontSize: '1rem' }}  // Reducir tamaño de fuente
-                >
-                    Iniciar sesión
+                <Link to="/login" style={{ textDecoration: 'none' }}>
+                    <button style={{ padding: '10px 15px', backgroundColor: '#003366', color: 'white', border: 'none', borderRadius: '5px' }}>
+                        Iniciar sesión
+                    </button>
                 </Link>
-                <Link 
-                    to="/register" 
-                    style={{ textDecoration: 'none', color: '#003366', fontSize: '1rem' }}  // Reducir tamaño de fuente
-                >
-                    Registrarse
+                <Link to="/register" style={{ textDecoration: 'none' }}>
+                    <button style={{ padding: '10px 15px', backgroundColor: '#003366', color: 'white', border: 'none', borderRadius: '5px' }}>
+                        Registrarse
+                    </button>
                 </Link>
             </div>
             
